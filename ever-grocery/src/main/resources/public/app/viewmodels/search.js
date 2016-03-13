fdbc100e61a95322d63e9dc849d38c3784d62279
Jasper Chua <jchua3508@gmail.com>
2016-03-13 10:29:48 +0800

    search update

--- ever-grocery/src/main/resources/public/app/viewmodels/search.js
+++ ever-grocery/src/main/resources/public/app/viewmodels/search.js
@@ -1,5 +1,42 @@
-define(['knockout'], function (ko) {
-    return {
-    	
-    };
+define(['durandal/app', 'knockout', 'modules/productservice'], function (app, ko, productService) {
+	var Product = function() {
+		this.productList = ko.observable();
+		
+		this.searchKey = ko.observable();
+		
+		this.itemsPerPage = ko.observable(app.itemsPerPage);
+		this.totalItems = ko.observable();
+		this.currentPage = ko.observable(1);
+		this.currentPageSubscription = null;
+	};
+	
+	Product.prototype.activate = function() {
+		var self = this;
+		
+		self.currentPage(1);
+		self.currentPageSubscription = self.currentPage.subscribe(function() {
+			self.refreshProductList();
+		});
+		
+		self.refreshProductList();
+	};
+	
+	Product.prototype.refreshProductList = function() {
+		var self = this;
+		
+		productService.getProductList(self.currentPage(), self.searchKey()).done(function(data) {
+			self.productList(data.list);
+			self.totalItems(data.total);
+		});
+	};
+	
+	Product.prototype.create = function() {
+		var self = this;
+		
+		ProductForm.show('Create', new Object()).then(function() {
+			self.refreshProductList();
+		});
+	};
+	
+    return Product;
 });
