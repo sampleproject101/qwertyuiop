e31f66b2956252677bd61e023e802def488356cb
Jasper Chua <jchua3508@gmail.com>
2016-04-03 21:25:16 +0800

    enum unitType

---
+++ ever-grocery/src/main/java/com/chua/evergrocery/enums/UnitType.java
@@ -0,0 +1,52 @@
+package com.chua.evergrocery.enums;
+
+public enum UnitType {
+	
+	BAG ("BAG"),
+	
+	BAR ("BAR"),
+	
+	BOTTLE ("BTL"),
+	
+	BOX ("BOX"),
+	
+	BUNDLE ("BDL"),
+	
+	CAN ("CAN"),
+	
+	CASE ("CS"),
+	
+	DOZEN ("DOZ"),
+	
+	JAR ("JAR"),
+	
+	PACK ("PACK"),
+	
+	PAIR ("PAIR"),
+	
+	PIECE ("PC"),
+	
+	POUCH ("POUCH"),
+	
+	REAM ("REAM"),
+	
+	SACK ("SACK"),
+	
+	TIE ("TIE"),
+	
+	TIN ("TIN"),
+	
+	X6 ("6s"),
+	
+	X8 ("8s");
+	
+	private final String shorthand;
+	
+	UnitType(final String shorthand) {
+		this.shorthand = shorthand;
+	}
+	
+	public String getShorthand() {
+		return shorthand;
+	}
+}
